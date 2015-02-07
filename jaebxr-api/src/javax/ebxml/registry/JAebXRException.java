f5612e8d1718cce75010139ff9367778523ef78b
Antonio Messina <messina@pa.icar.cnr.it>
2015-02-07 10:37:29 +0100

    JAebXR Exception class

---
+++ jaebxr-api/src/javax/ebxml/registry/JAebXRException.java
@@ -0,0 +1,29 @@
+package javax.ebxml.registry;
+
+import javax.xml.registry.JAXRException;
+
+public class JAebXRException extends JAXRException {
+
+	private static final long serialVersionUID = -2385204260584209299L;
+
+	public JAebXRException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public JAebXRException(String arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+	public JAebXRException(Throwable arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+	public JAebXRException(String arg0, Throwable arg1) {
+		super(arg0, arg1);
+		// TODO Auto-generated constructor stub
+	}
+
+}
