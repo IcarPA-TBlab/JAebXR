ff9b2d25389001fbbb8f0f090af52b701cb19854
Antonio Messina <messina@pa.icar.cnr.it>
2015-02-11 16:05:42 +0100

    Added createQuery and executeQuery for ebXML

--- jaebxr-api/src/javax/ebxml/registry/DeclarativeQueryManager.java
+++ jaebxr-api/src/javax/ebxml/registry/DeclarativeQueryManager.java
@@ -1,27 +1,37 @@
 package javax.ebxml.registry;
 
+import java.math.BigInteger;
+
+import javax.ebxml.registry.soap.BindingUtility;
 import javax.ebxml.registry.soap.SOAPMessenger;
+import javax.xml.bind.JAXBException;
 import javax.xml.registry.BulkResponse;
 import javax.xml.registry.InvalidRequestException;
 import javax.xml.registry.JAXRException;
 import javax.xml.registry.Query;
+import javax.xml.registry.RegistryException;
 import javax.xml.registry.UnsupportedCapabilityException;
+
+import org.oasis.ebxml.registry.bindings.query.AdhocQueryRequest;
+import org.oasis.ebxml.registry.bindings.query.ResponseOptionType;
+import org.oasis.ebxml.registry.bindings.query.ResponseOptionType.ReturnType;
+import org.oasis.ebxml.registry.bindings.rim.AdhocQueryType;
+import org.oasis.ebxml.registry.bindings.rim.QueryExpressionType;
+import org.oasis.ebxml.registry.bindings.rim.SlotListType;
+import org.oasis.ebxml.registry.bindings.rim.SlotType1;
+import org.oasis.ebxml.registry.bindings.rim.ValueListType;
+import org.oasis.ebxml.registry.bindings.rs.RegistryResponseType;
 
 public class DeclarativeQueryManager extends QueryManager implements javax.xml.registry.DeclarativeQueryManager {
 
 	private javax.xml.registry.DeclarativeQueryManager dqm = null;
-	@SuppressWarnings("unused")
+
 	private SOAPMessenger msgr = null;
 	
 	public DeclarativeQueryManager(javax.xml.registry.RegistryService rs) throws UnsupportedCapabilityException, JAXRException {
 		super();
 		this.dqm = rs.getDeclarativeQueryManager();
 		this.setRegistryService(rs);
-		this.setSOAPMessenger(ConfigurationFactory.getInstance().getSOAPMessenger());
-	}
-
-	protected void setSOAPMessenger(SOAPMessenger msgr) {
-		this.msgr = msgr;
 	}
 
 	public void setDeclarativeQueryManager(javax.xml.registry.DeclarativeQueryManager dqm) {
@@ -34,9 +44,77 @@ public class DeclarativeQueryManager extends QueryManager implements javax.xml.r
 		return dqm.createQuery(arg0, arg1);
 	}
 
+	public AdhocQueryType createQuery(String queryType, Object queryString) throws JAebXRException {
+		if ((!queryType.equals(CanonicalConstants.CANONICAL_QUERY_LANGUAGE_LID_ebRSFilterQuery)) &&
+		    (!queryType.equals(CanonicalConstants.CANONICAL_QUERY_LANGUAGE_LID_SQL_92)))
+				throw new JAebXRException("Unsupported query language: " + queryType);
+		
+		QueryExpressionType qet = rimFac.createQueryExpressionType();
+		qet.setQueryLanguage(queryType);
+		
+		if (queryType.equals(CanonicalConstants.CANONICAL_QUERY_LANGUAGE_LID_SQL_92))
+			qet.getContent().add(queryString);
+		else {
+			try {
+				qet.getContent().add(BindingUtility.getInstance().marshalObject(queryString));
+			} catch (JAXBException e1) {
+				throw new JAebXRException(e1);
+			}
+		}
+
+		AdhocQueryType aqt = rimFac.createAdhocQueryType();
+		
+		aqt.setId(createUUID());		
+		aqt.setQueryExpression(qet);
+
+		return aqt;
+	}
+	
 	@Override
 	public BulkResponse executeQuery(Query arg0) throws JAXRException {
 		return dqm.executeQuery(arg0);
 	}
 
+	public RegistryResponseType executeQuery(AdhocQueryType query) throws JAebXRException {
+		ResponseOptionType rot = queryFac.createResponseOptionType();
+		rot.setReturnComposedObjects(true);
+		rot.setReturnType(ReturnType.LEAF_CLASS);
+		
+		ValueListType vlt = rimFac.createValueListType();
+		vlt.getValue().add("true");
+		
+		SlotType1 st = rimFac.createSlotType1();
+		st.setName(CanonicalConstants.IMPL_SLOT_CREATE_HTTP_SESSION);
+		st.setValueList(vlt);
+		
+		SlotListType slt = rimFac.createSlotListType();
+		slt.getSlot().add(st);
+
+		AdhocQueryRequest aqr = queryFac.createAdhocQueryRequest();
+		
+		aqr.setId(createUUID());
+		
+		aqr.setMaxResults(BigInteger.valueOf(-1));
+		aqr.setStartIndex(BigInteger.valueOf(0));
+		aqr.setFederated(false);
+		
+		aqr.setAdhocQuery(query);
+		aqr.setResponseOption(rot);
+		aqr.setRequestSlotList(slt);
+
+		RegistryResponseType ebResp = null;
+
+		try {
+			RegistryResponseHolder resp = msgr.sendSoapRequest(BindingUtility.getInstance().marshalObject(aqr));
+			ebResp = resp.getRegistryResponseType();
+		} catch (JAXBException e) {
+			throw new JAebXRException(e);
+		} catch (RegistryException e) {
+			throw new JAebXRException(e);
+		} catch (JAXRException e) {
+			throw new JAebXRException(e);
+		}
+
+		return ebResp;
+	}
 }
